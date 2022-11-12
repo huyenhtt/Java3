526a883d45aa2599a1181aab19cac465cc9c3fe0
huyenhttph25539 <106076818+huyenhtt@users.noreply.github.com>
2022-11-12 10:14:05 +0700

    Firsst

---
+++ JavaApplication153/src/javaapplication153/JavaApplication153.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication153;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaApplication153 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello all!");
+    }
+    
+}
